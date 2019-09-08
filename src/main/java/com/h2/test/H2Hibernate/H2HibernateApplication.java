ae0e66d55f1b2ef09eacc50f9f7d7df18b24ca98
Rajeev <Rajeev@DESKTOP-UOMF93E>
2019-09-08 13:04:08 +0530

    Test

---
+++ src/main/java/com/h2/test/H2Hibernate/H2HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.h2.test.H2Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2HibernateApplication.class, args);
+	}
+
+}
